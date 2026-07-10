77825a022525f70137943ae2bc6ec0616812597d
Saqlain Afroz <sqn3680@gmail.com>
2026-07-10 22:56:43 +0530

    Updated rust tutorial

--- Rust/my_project/src/bin/scope.rs
+++ Rust/my_project/src/bin/scope.rs
@@ -16,4 +16,36 @@ fn main() {
     my_function();
 
     // println!("{}", message); // This will throw an error as message variable is out of scope
+    
+    let score = 80;
+
+    if score > 50 {
+        let result = "Pass";
+
+        println!("Result is {}", result);
+    }
+
+    // println!("Result is {}", result) // result is out of scope here
+
+    
+    // In Rust, you can declare a new variable with the same name in the same scope using let. This is called shadowing
+    
+    let x = 5;
+    let x = 10;
+
+    println!("x = {}", x);
+    
+    /*
+        Here, the two x variables are in different scopes. The inner x only exists inside the block. Outside the block, the original value remains.
+    */ 
+
+    let y = 8;
+
+    {
+        let y = 12;
+
+        println!("y inside the block {}", y)
+    }
+
+    println!("y outside the block {}", y)
 }
---
+++ Rust/my_project/src/bin/string.rs
@@ -0,0 +1,46 @@
+fn main() {
+    let greeting: &str = "Hello";
+
+    println!("{}", greeting);
+
+    /*
+        There are two main types of strings in Rust:
+
+            - &str - is called "string slices", and is used for fixed text like "Hello"
+            - String - used when you need a string that can change
+    */
+    
+    // You can create a `String` from a string literal using the `to_string()` method or the `String::from()` function
+    
+    let text1 = "Hello World".to_string();
+    let text2 = String::from("Hello World");
+
+    println!("{}", text1);
+    println!("{}", text2);
+    
+    /*
+        Strings are mutable, so you can change them if they are declared with mut.
+
+        Use push_str() to add text to a string
+    */
+
+    let mut greeting = String::from("Hello");
+    greeting.push_str(" World");
+    println!("{}", greeting);
+
+    // Use push() to add one character
+    
+    let mut word = String::from("Hi");
+    word.push('!');
+    println!("{}", word);
+
+    // You can combine strings using the format! macro
+    
+    let s1 = String::from("Hello");
+    let s2 = String::from("World!");
+    let s3 = String::from("What a beautiful day!");
+
+    let result = format!("{} {} {}", s1, s2, s3);
+
+    println!("{}", result);
+}
